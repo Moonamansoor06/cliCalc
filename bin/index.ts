f1081fabc92c49d69d9bc3fd5596d5fc8bb25da3
Moona mansoor <moonamansoor06@gmail.com>
2022-11-30 12:45:36 +0500

    initial commit

---
+++ bin/index.ts
@@ -0,0 +1,79 @@
+#! /usr/bin/env node
+
+
+import {question,prompt} from 'readline-sync'
+import clear from 'clear'
+import figlet from 'figlet';
+import colors from 'colors'
+import chalk from 'chalk'
+
+function main():void {
+  clear();
+  console.log(
+    chalk.bgGray(
+      figlet.textSync('Cli Calculator', { horizontalLayout: 'full' })
+    )
+  );
+  console.log("")
+let firstStr = question( colors.rainbow('Enter your First number \n', )) ;
+console.log("")
+
+
+let secondStr = question(colors.rainbow('Enter your Second number \n') );
+console.log("")
+console.log(colors.rainbow("Enter operator"))
+let operatorStr = prompt({limit:['+','-','*','/','%']}) ;
+
+
+
+const isOperator=(operatorStr:any):boolean=>{
+switch(operatorStr){
+  case '+':
+  case '-':
+  case '*':
+  case '/':
+  case '%':
+    return true;
+  default: return false  
+}
+}
+//const operator=isOperator(operatorStr)
+
+
+const isNumber=(str:string):boolean=>{
+  const convertToNum=parseInt(str)
+  const isNum:boolean=Boolean(!isNaN(convertToNum) )
+  return isNum
+}
+
+const firstNum=parseInt(firstStr)
+const secondNum=parseInt(secondStr)
+
+
+
+const isValidInput:boolean=isNumber(firstStr) && isOperator(operatorStr) && isNumber(secondStr)
+
+
+const calculate=(firstNum:number,secondNum:number,operatorStr:string):number=>{
+  switch(operatorStr){
+    case '+':
+      return firstNum+secondNum;
+    case '-':
+      return firstNum-secondNum;
+    case '*':
+      return firstNum*secondNum;
+    case '/':
+      return firstNum/secondNum;
+    case '%':
+      return firstNum%secondNum;
+    default: return 0  
+  }
+}
+
+ if(isValidInput){
+console.log(colors.rainbow(`${firstNum} ${operatorStr} ${secondNum} is ${calculate(firstNum,secondNum,operatorStr)}`)) 
+ }else console.log('something is not valid')
+}
+main()
+
+
